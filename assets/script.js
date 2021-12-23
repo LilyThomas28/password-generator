0b336acb21999daaecc078c2f8a8b03e6325eafd
Lily Thomas <lilylynettethomas@gmail.com>
2021-12-22 17:13:36 -0800

    Added last functions to js

--- assets/script.js
+++ assets/script.js
@@ -133,4 +133,26 @@ function charValidate(testChar) {
       }
       console.log("Generation: ", valCount);
       return generatedPassword;
+    }
+    // Something went wrong, end.
+    alert("Something went wrong.")
+    return "";
+  }
   
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    if (password !== "") {
+      passwordText.value = password;
+    }
+    else {
+      passwordText.value = "";
+      passwordText.placeholder = "Your Secure Password"
+    }
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
